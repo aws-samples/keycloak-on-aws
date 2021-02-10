f95723511a0715b5f24712dc25cb52dfc716934d
Charles Wei <wch@amazon.com>
2021-02-10 18:08:56 +0800

    refactor: main.ts

--- source/src/main.ts
+++ source/src/main.ts
@@ -4,12 +4,20 @@ import { KeycloakStack } from './stack';
 
 const app = new App();
 
-new KeycloakStack(app, 'keycloak-aurora-serverless-from-existing-vpc', { auroraServerless: true, fromExistingVPC: true, synthesizer: newSynthesizer() });
-new KeycloakStack(app, 'keycloak-aurora-serverless-from-new-vpc', { auroraServerless: true, fromExistingVPC: false, synthesizer: newSynthesizer() });
-new KeycloakStack(app, 'keycloak-from-existing-vpc', { auroraServerless: false, fromExistingVPC: true, synthesizer: newSynthesizer() });
-new KeycloakStack(app, 'keycloak-from-new-vpc', { auroraServerless: false, fromExistingVPC: false, synthesizer: newSynthesizer() });
+mkstack(app, 'keycloak-aurora-serverless-from-existing-vpc');
+mkstack(app, 'keycloak-aurora-serverless-from-new-vpc');
+mkstack(app, 'keycloak-from-existing-vpc');
+mkstack(app, 'keycloak-from-new-vpc');
 
 app.synth();
+
+function mkstack(a: App, id: string) {
+  return new KeycloakStack(a, id, {
+    auroraServerless: id.includes('aurora'),
+    fromExistingVPC: id.includes('existing-vpc'),
+    synthesizer: newSynthesizer(),
+  });
+}
 
 function newSynthesizer() {
   return process.env.USE_BSS ? new BootstraplessStackSynthesizer(): undefined;
